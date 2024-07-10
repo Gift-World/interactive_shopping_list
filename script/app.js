8ce6ad6573aa12b37b827f77be60e40c480a0b5e
charles Gift <charlesgiftangila@gmail.com>
2024-07-10 17:31:08 +0300

    implement clear list feature

--- script/app.js
+++ script/app.js
@@ -44,3 +44,10 @@ function addItem(event) {
   displayItems();
   event.target.reset();
 }
+const deleteButton = select(".delete");
+listen(deleteButton, "click", clearList);
+
+function clearList() {
+  shoppingList.length = 0;
+  displayItems();
+}
